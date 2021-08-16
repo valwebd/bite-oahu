7295b02c766d4e3d0031a8fd21579a9d6603e49e
valwebd <77312053+valwebd@users.noreply.github.com>
2021-08-16 08:23:32 -0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import Header from './components/Header'
-import List from './components/List'
-import Map from './components/Map'
+import Header from './components/Header/Header';
+import List from './components/List/List'
+import Map from './components/Map/Map'
 
 function App() {
   return (
-    <></>
+    <>
+    <Header/>
+    <List/>
+    <Map/>
+    </>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import useStyles from './styles';
 
 const Header = () => {
+  const classes = useStyles()
   return (
-    <div>
-      Header
-    </div>
+    <AppBar position='static'>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant='h5'>Bite Oahu</Typography>
+        <Box display='flex'>
+          
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder='Search...'
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+        </Box>
+      </Toolbar>
+    </AppBar>
   )
 }
 
